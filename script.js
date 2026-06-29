614c4995686bfb4ccb3f988977a2bf130ad8c431
JisooKang <toujours89@naver.com>
2026-06-29 11:17:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 console.log("script.js loaded");
 const CONFIG = {
   goalAmount: 294000,
-  currentAmount: 0,
-  supporterCount: 0,
+  currentAmount: 50000,
+  supporterCount: 1,
 
   bankName: "우리은행",
   accountNumber: "1002-959-495249",
